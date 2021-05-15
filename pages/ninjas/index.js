cca83d764692ec372022d529975c867a17b26dd6
MrNewaz <saifur.saif@northsouth.edu>
2021-05-16 04:20:52 +0600

    goodnight

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,25 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
