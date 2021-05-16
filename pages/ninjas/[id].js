fc48609d17d19db6fc400405886d39819912e3a9
MrNewaz <saifur.saif@northsouth.edu>
2021-05-16 20:42:06 +0600

    done

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -5,7 +5,7 @@ export const getStaticPaths = async () => {
   const paths = data.map((ninja) => {
     return {
       params: {
-        id: ninja.id,
+        id: ninja.id.toString(),
       },
     };
   });
@@ -15,10 +15,25 @@ export const getStaticPaths = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
